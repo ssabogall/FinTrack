c255cfe3b122b2f128532e0f846d8aca8b909fe3
Santiago Sabogal Lozano <125775758+ssabogall@users.noreply.github.com>
2026-03-06 18:56:26 -0500

    Revert "Feature/savings goals"

--- frontend/src/dtos/CreateGoalDTO.ts
+++
@@ -1,6 +0,0 @@
-import type { GoalInterface } from '@/interfaces/GoalInterface';
-
-export type CreateGoalDTO = Omit<
-  GoalInterface,
-  'id' | 'currentAmount' | 'status' | 'createdAt' | 'updatedAt' | 'transactionIds'
->;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,14 @@
 import './assets/css/input.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
 
-// internal imports
-import PiniaConfig from './PiniaConfig';
-
 const app = createApp(App);
-const pinia = PiniaConfig.init();
 
-app.use(pinia);
+app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
