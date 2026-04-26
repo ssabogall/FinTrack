57f69e3e048ae0ae91b942234e477c0f368b9bdc
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-04-25 21:27:00 -0500

    feat: auth module

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async login(email: string, password: string): Promise<{ access_token: string }> {
+        const user = await this.userService.findByEmail(email);
+            if (!user?.email) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = { sub: user.id, email: user.email };
+        return { access_token: await this.jwtService.signAsync(payload) };
+    }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -12,4 +12,44 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  private readonly users = [
+    {
+      id: 1,
+      name: 'Admin User',
+      email: 'admin@fintrack.local',
+      password: 'admin123',
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      categoryIds: null,
+      goalIds: null,
+      transactionIds: [],
+    },
+    {
+      id: 2,
+      name: 'Alex Johnson',
+      email: 'alex.johnson@fintrack.local',
+      password: 'password123',
+      role: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+  ]
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number): Promise<User | null> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  create(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
 }
