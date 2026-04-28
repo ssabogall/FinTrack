e521aedf95c99c3aee5798e98cb7454b93f22a40
lhiguitab <lhiguitab@eafit.edu.co>
2026-04-27 19:58:23 -0500

    refactor: delete class validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,14 +10,6 @@ async function bootstrap() {
   });
 
   app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
